e4c1f0ff47094f3cd38bc61c85094ee530a41894
mikevampm323 <mike.vartanyan@yahoo.com>
2020-06-21 08:58:37 -0700

    added navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,32 +1,45 @@
-import React, { useState, useEffect, useContext } from "react";
-// import { browsePlant } from "../../utils/API";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
-// import InputBase from "@material-ui/core/InputBase";
-// import Box from "@material-ui/core/Box";
-// import { fade, makeStyles } from "@material-ui/core/styles";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import SearchIcon from "@material-ui/icons/Search";
-// import AccountCircle from "@material-ui/icons/AccountCircle";
-// import Tooltip from "@material-ui/core/Tooltip";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   withRouter,
-// } from "react-router-dom";
-// import MenuListComposition from "../Menu";
-// import ForumIcon from "@material-ui/icons/Forum";
-// import EcoIcon from "@material-ui/icons/Eco";
-// import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
-// import plantContext from "../../utils/plantContext";
-// import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-// import userContext from "../../utils/userContext";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function Navbar() {
-  return <></>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            ABA Signs
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Navbar;
