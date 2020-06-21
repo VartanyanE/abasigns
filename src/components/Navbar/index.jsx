800adec3cdd5072ac3cfdd6035af9449c9add470
mikevampm323 <mike.vartanyan@yahoo.com>
2020-06-20 22:16:44 -0700

    boilerplate

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useContext } from "react";
+// import { browsePlant } from "../../utils/API";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import IconButton from "@material-ui/core/IconButton";
+// import Typography from "@material-ui/core/Typography";
+// import InputBase from "@material-ui/core/InputBase";
+// import Box from "@material-ui/core/Box";
+// import { fade, makeStyles } from "@material-ui/core/styles";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import SearchIcon from "@material-ui/icons/Search";
+// import AccountCircle from "@material-ui/icons/AccountCircle";
+// import Tooltip from "@material-ui/core/Tooltip";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   withRouter,
+// } from "react-router-dom";
+// import MenuListComposition from "../Menu";
+// import ForumIcon from "@material-ui/icons/Forum";
+// import EcoIcon from "@material-ui/icons/Eco";
+// import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+// import plantContext from "../../utils/plantContext";
+// import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+// import userContext from "../../utils/userContext";
+
+function Navbar() {
+  return <></>;
+}
+
+export default Navbar;
