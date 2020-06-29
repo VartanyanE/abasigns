dfa2112a1da47c2952205e62fa56433e845fbb1b
VartanyanE <mike.vartanyan@yahoo.com>
2020-06-28 21:43:23 -0700

    Updates

---
+++ precache-manifest.07a5ebf405c08d212592423730d977ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bda236a65ec880a61c9f398f0a7bb66",
+    "url": "/abasigns/index.html"
+  },
+  {
+    "revision": "51b33fe64e7745d35fbc",
+    "url": "/abasigns/static/css/main.4c87a413.chunk.css"
+  },
+  {
+    "revision": "7d525b3e8fdebe526910",
+    "url": "/abasigns/static/js/2.69e23934.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/abasigns/static/js/2.69e23934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b33fe64e7745d35fbc",
+    "url": "/abasigns/static/js/main.d97c0628.chunk.js"
+  },
+  {
+    "revision": "252264eeb5f992267a66",
+    "url": "/abasigns/static/js/runtime-main.42137865.js"
+  }
+]);
