23ac8bb6bc5e7aabd2cd8c7019b8d5d5c49dcfb8
mikevampm323 <mike.vartanyan@yahoo.com>
2020-06-29 00:38:38 -0700

    Updates

--- precache-manifest.39786f72495c48e32d422d307748e658.js
+++ precache-manifest.39786f72495c48e32d422d307748e658.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f9c76788164776ac0351654d709cad5",
+    "revision": "f39c9dfcd914a1a2a466413fe99297d7",
     "url": "/abasigns/index.html"
   },
   {
-    "revision": "17523e2142965bdaedc5",
+    "revision": "69733173ecd7dfb5102e",
     "url": "/abasigns/static/css/main.975a58a9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/abasigns/static/js/2.47e9fff0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "17523e2142965bdaedc5",
-    "url": "/abasigns/static/js/main.34473516.chunk.js"
+    "revision": "69733173ecd7dfb5102e",
+    "url": "/abasigns/static/js/main.19c7e625.chunk.js"
   },
   {
     "revision": "252264eeb5f992267a66",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abasigns/precache-manifest.315efb642aa90d38f73ddd9f85e1cae8.js"
+  "/abasigns/precache-manifest.39786f72495c48e32d422d307748e658.js"
 );
 
 self.addEventListener('message', (event) => {
