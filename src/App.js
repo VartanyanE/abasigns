ad0865bdddbfd1ec6a74d6ad90a3fd3b11d66420
mikevampm323 <mike.vartanyan@yahoo.com>
2020-06-29 18:52:14 -0700

    added some files to add routing to github pages

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
 import React, { useState, useContext } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Container from "@material-ui/core/Container";
 import Landing from "./pages/Landing";
-import About from "./pages/About"
+import About from "./pages/About";
 import { ThemeProvider, Paper } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core";
 import ModeContext from "./utils/ModeContext";
 
-
-
 function App() {
-
   const [darkMode, setDarkMode] = useState(false);
-  
-const theme = createMuiTheme({
-  palette: {
-    type: darkMode ? "dark" : "light",
-    primary: {
-      
-      main: "#bbdefb",
-      text: "#81c784"
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#bbdefb",
+        text: "#81c784",
+      },
+      secondary: {
+        main: "#f50057",
+        text: "#81c784",
+      },
+      success: {
+        main: "#bcbda4",
+      },
     },
-    secondary: {
-      main: "#f50057",
-      text: "#81c784"
-    },
-    success: {
-      main: "#bcbda4",
-      
-    }
-  },
- 
-});
-  
-  
+  });
+
   return (
-    <Router basename="/">
-      <ModeContext.Provider value={{darkMode, setDarkMode}}>
-      <ThemeProvider theme={theme}>
-      <Paper style={{height: "100vh"}}>
-      <div>
-        <Navbar />
-        <Container maxWidth="lg">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/about" component={About} />
-        </Container> 
-      </div>
-      </Paper>
-    </ThemeProvider>
-    </ModeContext.Provider>
-     </Router>
+    <Router basename={process.env.PUBLIC_URL}>
+      {/* <Switch> */}
+      <ModeContext.Provider value={{ darkMode, setDarkMode }}>
+        <ThemeProvider theme={theme}>
+          <Paper style={{ height: "100vh" }}>
+            <div>
+              <Navbar />
+              <Container maxWidth="lg">
+                <Route exact path="/" component={Landing} />
+                <Route path="/about" component={About} />
+              </Container>
+            </div>
+          </Paper>
+        </ThemeProvider>
+      </ModeContext.Provider>
+      {/* </Switch> */}
+    </Router>
   );
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,22 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import ImgMediaCard from "../components/Card";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "../pages/About"
-import Icon from '@material-ui/core/Icon';
-import Box from '@material-ui/core/Box';
-import SaveIcon from '@material-ui/icons/Save';
-
-
+import About from "../pages/About";
+import Icon from "@material-ui/core/Icon";
+import Box from "@material-ui/core/Box";
+import SaveIcon from "@material-ui/icons/Save";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    
-   
   },
   paper: {
     padding: theme.spacing(2),
@@ -39,64 +35,78 @@ const useStyles = makeStyles((theme) => ({
   buttonCenter: {
     display: "flex",
     justifyContent: "center",
-    color: theme.palette.success.light
-    },
-    
-  buttonStyle : {
-    // color: theme.palette.success.light
-  }
-}));
+  },
 
+  buttonStyle: {
+    color: theme.palette.success.light,
+  },
+}));
 
 function Landing() {
   const classes = useStyles();
-  const [showButton, setShowButton]= useState(false);
+  const [showButton, setShowButton] = useState(false);
 
-  
-
-
+  const handleClick = () => {
+    if (showButton === false) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
   return (
     <Router>
-    <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid item xs={2} sm={5}></Grid>
-          <Grid item xs={8} sm={2}  >
-          <Box className={classes.buttonCenter} > <Button className={classes.buttonStyle} variant="contained" color="primary" href="/about" >
-        Landing
-      </Button>
-      </Box>
+      <div className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={2} sm={5}></Grid>
+          <Grid item xs={8} sm={2}>
+            <Box className={classes.buttonCenter}>
+              {" "}
+              <Paper elevation="10 ">
+                <Button
+                  className={classes.buttonStyle}
+                  variant="contained"
+                  color="primary"
+                  href={process.env.PUBLIC_URL + "/about"}
+                >
+                  Landing
+                </Button>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid item xs={2} sm={5}></Grid>
+          <Grid item xs={6}>
+            {showButton ? (
+              ""
+            ) : (
+              <Button variant="outlined" color="secondary">
+                Secondary
+              </Button>
+            )}
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+              endIcon={<SaveIcon>send</SaveIcon>}
+            >
+              Save
+            </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Card</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Card</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Card</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Card</Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={2} sm={5}></Grid>
-        <Grid item xs={6} >
-       {showButton ? "" :  <Button variant="contained" color="primary" className={classes.buttonStyle}  >
-        Secondary 
-  </Button> }
-        </Grid>
-        <Grid item xs={6}>
-       <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setShowButton(!showButton)}
-       
-        endIcon={<SaveIcon>send</SaveIcon>}
-      >
-        Save
-      </Button>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Card</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Card</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Card</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>Card</Paper>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
     </Router>
   );
 }
